05dd4d064ae5e1d4ba25bbb90dbb86c49b923663
zhang zheng <zheng.zhang@sieredu.com>
2025-03-12 17:48:14 +0800

    Initial Commit

---
+++ SALabel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SALabel
+//
+//  Created by zhangz on 2025/3/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
